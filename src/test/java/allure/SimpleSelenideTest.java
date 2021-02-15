958e63e45a98de6fb34aadbbe63512110f762685
Yuliya <9757975@gmail.com>
2021-02-15 12:28:25 +0300

    add url static

--- src/test/java/allure/SimpleSelenideTest.java
+++ src/test/java/allure/SimpleSelenideTest.java
@@ -1,7 +1,11 @@
 package allure;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -12,14 +16,24 @@ import static com.codeborne.selenide.Condition.visible;
 
 public class SimpleSelenideTest {
 
+    private static final String BASE_URL = "http://github.com";
     private final static String REPOSITORY = "chulietta/qa_guru-4_5";
     private final static String ISSUE_NAME = "Добавить тест";
 
     @Test
-    void simpleSelenideTest() {
+    @Owner("koronkevich")
+    @Severity(SeverityLevel.CRITICAL)
+    @Tags({@Tag("web"), @Tag("critical")})
+    @Link(name = "Base URL", value = BASE_URL)
+
+    @Feature("Issues")
+    @Story("Поск Issue")
+    @DisplayName("Поиск Issue по названию в репозитории")
+
+    public void simpleSelenideTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        open("https://github.com");
+        open(BASE_URL);
         $(".header-search-input").setValue(REPOSITORY).pressEnter();
         $(By.linkText(REPOSITORY)).click();
         $(withText("Issues")).click();
